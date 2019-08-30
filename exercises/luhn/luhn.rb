26d9464dcbf76c7234cc2596e8a03d69670dea5b
Ethan Herbertson <ejherbertson@msts.com>
2019-08-30 15:38:52 -0500

    Tweaked Luhn solution to use algorithm as described in problem

--- exercises/luhn/luhn.rb
+++ exercises/luhn/luhn.rb
@@ -11,9 +11,9 @@ module Luhn
     # Here's one way to do it that allows for nice, easy-to-follow comments:
     #
     normalized                                    #         Start with the normalized string : "055444285"
-      .chars.reverse.map(&:to_i)                  #     Consider the digits in reverse order : [5,    8, 2, 4, 4, 4, 5,    5, 0]
-      .map.with_index {|d, i| d * (1 + (i % 2))}  #                Double every second digit : [5,   16, 2, 8, 4, 8, 5,   10, 0]
-      .map(&:to_s).join.chars.map(&:to_i)         #    Consider all of THOSE numbers' digits : [5, 1, 6, 2, 8, 4, 8, 5, 1, 0, 0]
+      .chars.reverse.map(&:to_i)                  #     Consider the digits in reverse order : [5,  8, 2, 4, 4, 4, 5,  5, 0]
+      .map.with_index {|d, i| d * (1 + (i % 2))}  #                Double every second digit : [5, 16, 2, 8, 4, 8, 5, 10, 0]
+      .map {|d| d > 9 ? d - 9 : d }               #         Subtract 9 from any that are > 9 : [5,  7, 2, 8, 4, 8, 5,  1, 0]
       .reduce(&:+)                                #                          Sum all of them : 40
       .modulo(10) == 0                            # Validate if that sum is a multiple of 10 : true
   end
